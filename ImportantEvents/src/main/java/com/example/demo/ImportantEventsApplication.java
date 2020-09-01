0dd97710c099d83bf5908a583e1ecdd1f430c6a9
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-09-01 14:00:03 -0700

    Add files via upload

---
+++ ImportantEvents/src/main/java/com/example/demo/ImportantEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportantEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImportantEventsApplication.class, args);
+    }
+
+}
