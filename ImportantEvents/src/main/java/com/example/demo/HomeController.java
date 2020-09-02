6f44d2015b99566498a9d9bff8572b06fec92bce
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-09-01 20:46:20 -0700

    Add files via upload

--- ImportantEvents/src/main/java/com/example/demo/HomeController.java
+++ ImportantEvents/src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,10 +31,29 @@ public class HomeController {
 
     @PostMapping("/process")
     public String processTaskForm(@Valid Task task, BindingResult result) {
-        if ( result.hasErrors() ) {
+        if (result.hasErrors()) {
             return "taskform";
         }
         taskRepository.save(task);
         return "redirect:/";
     }
+
+    @RequestMapping("/detail/{id}")
+    public String showTask(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("task", taskRepository.findById(id).get());
+        return "showTasks";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateTask(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("task", taskRepository.findById(id).get());
+        return "taskform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id) {
+        taskRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
+
