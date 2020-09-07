f76829ba3f270913448a8d8b77a8b9b2eefda04f
Wali Morris <walimmorris@gmail.com>
2020-09-06 18:31:01 -0700

    updated comments

--- ImportantEvents/src/main/java/com/example/demo/ImportantEventsApplication.java
+++ ImportantEvents/src/main/java/com/example/demo/ImportantEventsApplication.java
@@ -3,9 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ImportantEvent project tasks may or may not be very important, but it's a simple
+ * example of a To-do list using Java Springboot.
+ *
+ * @author Wali Morris
+ * @since 09/05/2020
+ */
+
 @SpringBootApplication
 public class ImportantEventsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ImportantEventsApplication.class, args);
     }
