00bda2d17aa2d8ce44e403b238e346a57f1187b2
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-09-06 18:33:17 -0700

    Update ImportantEventsApplication.java

--- ImportantEvents/src/main/java/com/example/demo/ImportantEventsApplication.java
+++ ImportantEvents/src/main/java/com/example/demo/ImportantEventsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * ImportantEvent project tasks may or may not be very important, but it's a simple
  * example of a To-do list using Java Springboot.
  *
- * @author Wali Morris
+ * @author Wali Morris<walimmorris@gmail.com>
  * @since 09/05/2020
  */
 
